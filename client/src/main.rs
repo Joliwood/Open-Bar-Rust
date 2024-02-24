e8cccde3f63fcadf811cf693f1db60f8f5af5ec3
Guillaume Jolibois <jolibois.contact@gmail.com>
2024-02-24 19:54:42 +0100

    :construction: vertical movements added + more SOC

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,8 +1,42 @@
-use bevy::prelude::*;
+mod systems;
+
+use bevy::{ prelude::*, math::vec3 };
+use systems::constants::*;
+use systems::movements::paddle_movements::paddle_movements;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        // Background color
+        .insert_resource(ClearColor(Color::rgb(0.9, 0.9, 0.9)))
         .add_systems(Update, bevy::window::close_on_esc)
+        // This startup will start only when the game starts
+        .add_systems(Startup, setup)
+        .add_systems(FixedUpdate, paddle_movements)
         .run();
+}
+
+#[derive(Component)]
+
+pub struct Paddle;
+
+fn setup(mut commands: Commands) {
+    // Camera
+    commands.spawn(Camera2dBundle::default());
+
+    // Paddle
+    commands.spawn((SpriteBundle {
+        transform: Transform {
+            translation: vec3(0.0, PADDLE_START_Y, 0.0),
+            ..default()
+        },
+        sprite: Sprite {
+            custom_size: Some(PADDLE_SIZE),
+            color: PADDLE_COLOR,
+            ..default()
+        },
+        ..default()
+        },
+        Paddle,
+    ));
 }
---
+++ client/src/systems/constants.rs
@@ -0,0 +1,6 @@
+use bevy::prelude::{Color, Vec2};
+
+pub const PADDLE_SIZE: Vec2 = Vec2::new(20.0, 20.0);
+pub const PADDLE_COLOR: Color = Color::rgb(0.3, 0.3, 0.7);
+pub const PADDLE_START_Y: f32 = 0.0;
+pub const PADDLE_SPEED: f32 = 500.0;
