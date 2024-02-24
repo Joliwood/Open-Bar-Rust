a520fae34f7612c9d7e9c07974114c31626d8273
Guillaume Jolibois <jolibois.contact@gmail.com>
2024-02-24 20:35:47 +0100

    :hammer: 2d init scene changed to 3d version without movements

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,8 +1,11 @@
 mod systems;
 
-use bevy::{ prelude::*, math::vec3 };
-use systems::constants::*;
-use systems::movements::paddle_movements::paddle_movements;
+use bevy::{ 
+    prelude::*, 
+    // math::vec3 
+};
+// use systems::constants::*;
+// use systems::movements::paddle_movements::paddle_movements;
 
 fn main() {
     App::new()
@@ -12,31 +15,68 @@ fn main() {
         .add_systems(Update, bevy::window::close_on_esc)
         // This startup will start only when the game starts
         .add_systems(Startup, setup)
-        .add_systems(FixedUpdate, paddle_movements)
+        // .add_systems(FixedUpdate, paddle_movements)
         .run();
 }
 
-#[derive(Component)]
+// #[derive(Component)]
 
-pub struct Paddle;
+// pub struct Paddle;
 
-fn setup(mut commands: Commands) {
-    // Camera
-    commands.spawn(Camera2dBundle::default());
+// 2D basic setup
+// fn setup(mut commands: Commands) {
+//     // Camera
+//     commands.spawn(Camera2dBundle::default());
 
-    // Paddle
-    commands.spawn((SpriteBundle {
-        transform: Transform {
-            translation: vec3(0.0, PADDLE_START_Y, 0.0),
-            ..default()
-        },
-        sprite: Sprite {
-            custom_size: Some(PADDLE_SIZE),
-            color: PADDLE_COLOR,
-            ..default()
-        },
+//     // Paddle
+//     commands.spawn((SpriteBundle {
+//         transform: Transform {
+//             translation: vec3(0.0, PADDLE_START_Y, 0.0),
+//             ..default()
+//         },
+//         sprite: Sprite {
+//             custom_size: Some(PADDLE_SIZE),
+//             color: PADDLE_COLOR,
+//             ..default()
+//         },
+//         ..default()
+//         },
+//         Paddle,
+//     ));
+// }
+
+/// set up a simple 3D scene
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // circular base
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Circle::new(4.0)),
+        material: materials.add(Color::WHITE),
+        transform: Transform::from_rotation(Quat::from_rotation_x(-std::f32::consts::FRAC_PI_2)),
         ..default()
+    });
+    // cube
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Cuboid::new(1.0, 1.0, 1.0)),
+        material: materials.add(Color::rgb_u8(124, 144, 255)),
+        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+        ..default()
+    });
+    // light
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            shadows_enabled: true,
+            ..default()
         },
-        Paddle,
-    ));
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..default()
+    });
+    // camera
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
 }
--- client/src/systems/movements/paddle_movements.rs
+++ client/src/systems/movements/paddle_movements.rs
@@ -1,47 +1,47 @@
-use bevy::prelude::*;
-use crate::Paddle;
-use super::super::constants::*;
+// use bevy::prelude::*;
+// use crate::Paddle;
+// use super::super::constants::*;
 
-pub fn paddle_movements(
-  keyboard_input: Res<ButtonInput<KeyCode>>,
-  time: Res<Time>,
-  mut query: Query<&mut Transform, With<Paddle>>,
-) {
-  let mut paddle_transform = query.single_mut();
-  let mut horizontal_direction = 0.0;
-  let mut vertical_direction = 0.0;
+// pub fn paddle_movements(
+//   keyboard_input: Res<ButtonInput<KeyCode>>,
+//   time: Res<Time>,
+//   mut query: Query<&mut Transform, With<Paddle>>,
+// ) {
+//   let mut paddle_transform = query.single_mut();
+//   let mut horizontal_direction = 0.0;
+//   let mut vertical_direction = 0.0;
 
-  // Left movement (Azerty only)
-  if keyboard_input.pressed(KeyCode::KeyA) 
-  || keyboard_input.pressed(KeyCode::ArrowLeft) {
-    horizontal_direction -= 1.0;
-  }
-  // Right movement
-  if keyboard_input.pressed(KeyCode::KeyD)
-  || keyboard_input.pressed(KeyCode::ArrowRight) {
-    horizontal_direction += 1.0;
-  }
+//   // Left movement
+//   if keyboard_input.pressed(KeyCode::KeyA) 
+//   || keyboard_input.pressed(KeyCode::ArrowLeft) {
+//     horizontal_direction -= 1.0;
+//   }
+//   // Right movement
+//   if keyboard_input.pressed(KeyCode::KeyD)
+//   || keyboard_input.pressed(KeyCode::ArrowRight) {
+//     horizontal_direction += 1.0;
+//   }
 
-  // Up movement
-  if keyboard_input.pressed(KeyCode::KeyW)
-  || keyboard_input.pressed(KeyCode::ArrowUp) {
-    vertical_direction += 1.0;
-  }
+//   // Up movement
+//   if keyboard_input.pressed(KeyCode::KeyW)
+//   || keyboard_input.pressed(KeyCode::ArrowUp) {
+//     vertical_direction += 1.0;
+//   }
 
-  // Down movement
-  if keyboard_input.pressed(KeyCode::KeyS)
-  || keyboard_input.pressed(KeyCode::ArrowDown) {
-    vertical_direction -= 1.0;
-  }
+//   // Down movement
+//   if keyboard_input.pressed(KeyCode::KeyS)
+//   || keyboard_input.pressed(KeyCode::ArrowDown) {
+//     vertical_direction -= 1.0;
+//   }
 
-  // Calculate the new horizontal paddle position based on player input
-  let new_paddle_horizontal_position = 
-    paddle_transform.translation.x + horizontal_direction * PADDLE_SPEED * time.delta_seconds();
+//   // Calculate the new horizontal paddle position based on player input
+//   let new_paddle_horizontal_position = 
+//     paddle_transform.translation.x + horizontal_direction * PADDLE_SPEED * time.delta_seconds();
 
-  // Calculate the new vertical paddle position based on player input
-  let new_paddle_vertical_position = 
-    paddle_transform.translation.y + vertical_direction * PADDLE_SPEED * time.delta_seconds();
+//   // Calculate the new vertical paddle position based on player input
+//   let new_paddle_vertical_position = 
+//     paddle_transform.translation.y + vertical_direction * PADDLE_SPEED * time.delta_seconds();
 
-  paddle_transform.translation.x = new_paddle_horizontal_position;
-  paddle_transform.translation.y = new_paddle_vertical_position;
-}
+//   paddle_transform.translation.x = new_paddle_horizontal_position;
+//   paddle_transform.translation.y = new_paddle_vertical_position;
+// }
